a75154c112d56272f13695cba5010d59bcde7a77
Vlazan Madalin <madalinvlazan@gmail.com>
2026-01-05 17:03:40 +0200

    Create getUserInfo function

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log("JavaScript Homework Handling User Input, Data Types, and Arrays");
+
+// Function get user info
+
+function getUserInfo() {
+  const name = prompt("Please enter student name:");
+  const age = prompt("Please enter student age:");
+  const attendance = confirm(
+    "Are student present today? Click OK for Yes, Cancel for No."
+  );
+
+  return { name, age, attendance };
+}
+
+getUserInfo();
